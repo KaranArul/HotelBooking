89b4712843ad55d005c1212a0e95799d156dd2c5
KaranArul <manikandanarul1122@gmail.com>
2022-09-30 19:54:31 +0530

    First Time Push

---
+++ src/org/info/PrintNumbers.java
@@ -0,0 +1,18 @@
+package org.info;
+
+import java.lang.reflect.Method;
+
+public class PrintNumbers {
+
+	private void method1() {
+	System.out.println("me1");
+	}
+	private void method2() {
+     System.out.println("me2");
+	}
+	private void method3() {
+     System.out.println("me3");
+	}
+		
+	}
+
